94ffa176e79e823a07fa57617d9907c864710c99
Pembe Karahan <pembek@gmail.com>
2022-01-10 18:48:53 +0300

    html form fix

--- contracts/TodooList.sol
+++ contracts/TodooList.sol
@@ -15,9 +15,16 @@ contract TodooList {
 
   mapping(uint => Task) public tasks;
 
+  event TaskCreated(
+    uint id,
+    string content,
+    bool completed
+  );
+
   function createTask(string memory _content) public {
     taskCount ++;
     tasks[taskCount] = Task(taskCount, _content, false);
+    emit TaskCreated(taskCount, _content, false);
   }
 
 }
--- truffle-config.js
+++ truffle-config.js
@@ -1,20 +1,16 @@
+
 module.exports = {
   networks: {
     development: {
-      host: "localhost",
+      host: "127.0.0.1",
       port: 7545,
-      network_id: "*", // Match any network id
-      gas: 5000000
+      network_id: "*" // Match any network id
     }
   },
-  compilers: {
-    solc: {
-      settings: {
-        optimizer: {
-          enabled: true, // Default: false
-          runs: 200      // Default: 200
-        },
-      }
+  solc: {
+    optimizer: {
+      enabled: true,
+      runs: 200
     }
   }
-};
+}
